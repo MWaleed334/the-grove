1c9637a201532dbb056c88ddce633a856448ac68
Waleed Ahsan <waleedahsan226@gmail.com>
2025-08-13 00:13:29 -0600

    test grove develop

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,7 +4,6 @@ import Footer from '@/components/footer/Footer'
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-dvh flex flex-col bg-gradient-to-b from-[#283606] to-[#0F1500]">
-      {/* <Navbar /> */}
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
